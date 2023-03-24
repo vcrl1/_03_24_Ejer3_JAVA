16794d06ce08c47acfa73269c0a969faee585613
vcrl1 <vcrl1994@gmail.com>
2023-03-24 12:12:20 +0100

    Ejercicio 3 sin atributos

---
+++ src/com/adecco/modelo/Area.java
@@ -0,0 +1,12 @@
+package com.adecco.modelo;
+
+public class Area {
+
+    public float areaCirculo(float radio){
+        return radio*radio*3.14f;
+    }
+    public float areaRectangulo(float base, float altura){
+        return base*altura;
+
+    }
+}
---
+++ src/com/adecco/presentacion/Main.java
@@ -0,0 +1,14 @@
+package com.adecco.presentacion;
+
+import com.adecco.modelo.Area;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Area a1= new Area();
+        System.out.println(a1.areaCirculo(2f));
+        System.out.println(a1.areaRectangulo(5f,8f));
+//        Area a2=new Area();
+//        System.out.println(a2);
+    }
+}
